43229e9996178ec37a26fd7818142e2db227ecfb
Jism5 <88894047+Jism5@users.noreply.github.com>
2021-08-19 12:21:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+// opem menubar
+
+const openMenu = document.querySelector('.hamburger');
+const Menu = document.querySelector('.main-nav');
+
+
+openMenu.addEventListener('click', () => {
+    Menu.classList.toggle('show');
+})
+
+// get in touch button
+const btnG = document.querySelector('btng');
+const contactForm = document.querySelector('contact-section');
+
